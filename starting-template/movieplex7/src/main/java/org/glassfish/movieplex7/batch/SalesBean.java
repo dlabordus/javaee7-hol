a104486e75ad08167854d9be2f3af549bbe24425
Dennis Labordus <dlabordus@vxcompany.com>
2013-11-17 17:24:24 +0100

    Chapter 4 & 5

---
+++ starting-template/movieplex7/src/main/java/org/glassfish/movieplex7/batch/SalesBean.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.glassfish.movieplex7.batch;
+
+import java.util.List;
+import java.util.Properties;
+import javax.batch.operations.JobOperator;
+import javax.batch.operations.JobStartException;
+import javax.batch.runtime.BatchRuntime;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import org.glassfish.movieplex7.entities.Sales;
+
+/**
+ *
+ * @author dlabordus
+ */
+@Named
+@RequestScoped
+public class SalesBean {
+
+    @PersistenceUnit
+    EntityManagerFactory emf;
+
+    public void runJob() {
+        try {
+            JobOperator jo = BatchRuntime.getJobOperator();
+            long jobId = jo.start("eod-sales", new Properties());
+            System.out.println("Started job: with id: " + jobId);
+        } catch (JobStartException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public List<Sales> getSalesData() {
+        return emf.createEntityManager().createNamedQuery("Sales.findAll", Sales.class).getResultList();
+    }
+}
